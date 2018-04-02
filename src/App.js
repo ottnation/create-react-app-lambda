73c0c8ec5e5cefe4e1ba2b8144cca200fd79658d
Sumit Shah <sumitshah@gmail.com>
2018-04-02 17:21:26 -0500

    First commit for NEtflity tutorial on Identity and Functions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class LambdaDemo extends Component {
+/**class LambdaDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {loading: false, msg: null};
@@ -25,6 +25,47 @@ class LambdaDemo extends Component {
       <span>{msg}</span>
     </p>
   }
+}**/
+class SlackMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, text: null, error: null, success: false};
+  }
+  handleText = (e) => {
+    this.setState({text: e.target.value});
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({loading: true});
+    fetch('/.netlify/functions/slack', {
+      method: "POST",
+      body: JSON.stringify({
+        text: this.state.text
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        return response.text().then(err => {throw(err)});
+      }
+    })
+    .then(() => this.setState({loading: false, text: null, success: true, error: null}))
+    .catch(err => this.setState({loading: false, success: false, error: err.toString()}))
+  }
+  render() {
+    const {loading, text, error, success} = this.state;
+    return <form onSubmit={this.handleSubmit}>
+      {error && <p><strong>Error sending message: {error}</strong></p>}
+      {success && <p><strong>Done! Message sent to Slack</strong></p>}
+      <p>
+        <label>Your Message: <br/>
+          <textarea onChange={this.handleText} value={text}></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit" disabled={loading}>{loading ? "Sending Slack Message..." : "Send a Slack Message"}</button>
+      </p>
+    </form>;
+  }
 }
 
 class App extends Component {
@@ -32,13 +73,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Slack Messenger</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo/>
+        <SlackMessage/>
       </div>
     );
   }
